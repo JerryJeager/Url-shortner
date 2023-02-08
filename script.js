fa41b52388f172f865ffc0ac0c751aaf98a6ec44
Amadi Jerry <amadijerry310@gmail.com>
2023-02-08 22:30:33 +0100

    completed hero section

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const header = document.querySelector('header')
 const hamburger = document.querySelector('.hamburger')
 const menuLine1 = document.querySelector('.line1')
 const menuLine2 = document.querySelector('.line2')
@@ -6,10 +7,18 @@ const navList = document.querySelector('.nav-list')
 
 
 
-
 hamburger.addEventListener('click', () => {
     menuLine1.classList.toggle('line1-close')
     menuLine2.classList.toggle('line2-close')
     menuLine3.classList.toggle('line3-close')
     navList.classList.toggle('nav-list-closed')
+})
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 30) {
+        header.classList.add("header-fixed")
+    }
+    else {
+        header.classList.remove("header-fixed")
+    }
 })
