31b12f76f2c8a5ccb2c6d2257dffd9ac64e9accb
Amadi Jerry <amadijerry310@gmail.com>
2023-02-11 18:34:45 +0100

    added shortcode api

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const menuLine1 = document.querySelector('.line1')
 const menuLine2 = document.querySelector('.line2')
 const menuLine3 = document.querySelector('.line3')
 const navList = document.querySelector('.nav-list')
-
+// const longLink = document.querySelector('input[name="long-link"]')
+// const shortLinkBtn = document.querySelector('.shorten-btn').querySelector('button')
+// const shortLinks = document.querySelector('.short-links')
+const copyShortLink = document.querySelector('.copy-btn')
 
 
 hamburger.addEventListener('click', () => {
@@ -21,4 +24,49 @@ window.addEventListener('scroll', () => {
     else {
         header.classList.remove("header-fixed")
     }
+})
+
+const longLink = document.querySelector('input[name="long-link"]')
+const shortLinkBtn = document.querySelector('.shorten-btn').querySelector('button')
+const shortLinks = document.querySelector('.short-links')
+
+
+
+async function shortenLink(longURL) {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiEndpoint = `${proxyUrl}https://api.shrtco.de/v2/shorten`;
+    const response = await fetch(apiEndpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            url: longURL
+        })
+    });
+    if(response.status !== 200){
+        throw new Error('epp me e dey carry me where i nor know')
+    }
+    const data = await response.json();
+    return data.shortLink;
+}
+
+
+shortLinkBtn.addEventListener('click', () => {
+    shortenLink(longLink.value).then(shortURL => {
+        console.log(`Short URL: ${shortURL}`);
+        console.log(longLink.value)
+        shortLinks.innerHTML += `<div class="shortened-link">
+                <div class="old-link">
+                    <p>${oldLink}</p>
+                </div>
+                <div class="new-link">
+                    <p>${data}</p>
+                    <button class="copy-btn">Copy</button>
+                </div>
+            </div>`
+    }).catch(error => {
+        console.log(error);
+        console.log(longLink.value)
+    });
 })
